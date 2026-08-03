d17bf381b017b9b970eaa1fc856d7d3b089194c2
RAKSHIT CHAUDHARY <rakshitchaudhary323@gmail.com>
2026-08-03 13:13:24 +0530

    Time: 12 ms (86.94%) | Memory: 136.2 MB (76.04%) - LeetSync

---
+++ 1522-stone-game-iii/stone-game-iii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string stoneGameIII(vector<int>& stoneValue) {
+        
+        int n = stoneValue.size();
+
+        vector<int> dp(n + 1, 0);
+
+        // dp[n] = 0 because no stones left
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            int take = 0;
+            dp[i] = INT_MIN;
+
+            // take 1,2,3 stones
+            for (int k = 0; k < 3 && i + k < n; k++) {
+
+                take += stoneValue[i + k];
+
+                dp[i] = max(dp[i], take - dp[i + k + 1]);
+            }
+        }
+
+
+        if (dp[0] > 0)
+            return "Alice";
+
+        else if (dp[0] < 0)
+            return "Bob";
+
+        else
+            return "Tie";
+    }
+};
